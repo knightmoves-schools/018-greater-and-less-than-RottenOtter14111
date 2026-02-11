a1f4e0335fe676a520a7576c969b48233a8e48e5
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-11 16:27:08 +0000

    dine

--- Verifier.cs
+++ Verifier.cs
@@ -1,5 +1,39 @@
 ﻿namespace knightmoves;
 public class Verifier
 {
-    // Add your code here
+    public string OldEnough(int age)
+    {
+        if (age > 17)
+        {
+            return "old enough";
+        }
+        return "";
+    }
+
+    public string UnderAge(int age)
+    {
+        if (age < 18)
+        {
+            return "under age";
+        }
+        return "";
+    }
+
+    public string LegalSpeed(int speed)
+    {
+        if (speed <= 50)
+        {
+            return "legal speed";
+        }
+        return "";
+    }
+
+    public string IsGoldenYears(int age)
+    {
+        if (age >= 65)
+        {
+            return "golden years";
+        }
+        return "";
+    }
 }
